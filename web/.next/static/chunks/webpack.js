348dbcc4a36ec250d0de53b03b63480ff578a558
Peter Laxalt <pete@laxaltandmciver.co>
2020-11-26 16:08:31 -0500

    logs

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e9fd749a8dc67e81ef81";
+/******/ 	var hotCurrentHash = "22b7631d87368456b24c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
