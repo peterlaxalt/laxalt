78a62c0bdd191d30aa210a5be9353bb637f668b9
Peter Laxalt <peter@laxa.lt>
2022-01-18 23:50:15 -0500

    responsive stuff

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3f5d474d533054f3faf6";
+/******/ 	var hotCurrentHash = "ce4a17c885f6af500677";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- web/components/FilterFrameHeader/styles.scss.tsx
+++ web/components/FilterFrameHeader/styles.scss.tsx
@@ -543,13 +543,14 @@ export const InteractiveLogotypeStyle = styled.div`
 
     position: fixed;
 
-    top: 50%;
+    top: calc(${Root.Nav.Size} * 1.5);
     left: 50%;
 
     transform: translate(-50%, -50%);
 
     width: 100vw;
-    height: 100vh;
+    height: calc(${Root.Nav.Size} * 3);
+    /* height: 100vh; */
 
     overflow: hidden;
 
@@ -560,7 +561,7 @@ export const InteractiveLogotypeStyle = styled.div`
     /* mix-blend-mode: exclusion; */
 
     @media(max-width: ${Theme.Base.Media.Width.Sm}) {
-      height: 100vh;
+      top: calc(${Root.Nav.Size} * 1.66);
     }
     
     .${InteractiveLogotypeClassName}__el__circle {
